1648eccbe5abbe64c9138b158bcc2535e8dc6cd6
Mike Zahalan <mzahalan@amazon.com>
2025-06-12 21:48:56 -0700

    Lesson 10 - Imports and Refactor
    moved the home screen to it's own file in a screens directory.
    
    had Q updated my linter config with some goodies.

--- coffee_card/lib/main.dart
+++ coffee_card/lib/main.dart
@@ -1,36 +1,8 @@
+import 'package:coffee_card/screens/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text('My Coffee Id'),
-        backgroundColor: Colors.brown[700],
-        centerTitle: true,
-      ),
-      backgroundColor: Colors.blueGrey,
-      body: Home(),
-    ),
+  runApp(const MaterialApp(
+    home: Home(),
   ));
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.orange,
-      padding: const EdgeInsets.all(20),
-      margin: const EdgeInsets.fromLTRB(10,40,0,0),
-      child: const Text('Hello from flutter', style: TextStyle(
-        color: Colors.white,
-        fontSize: 20,
-        letterSpacing: 4,
-        decoration: TextDecoration.underline,
-        fontWeight: FontWeight.bold,
-        fontStyle: FontStyle.italic
-      ),),
-    );
-  }
 }
