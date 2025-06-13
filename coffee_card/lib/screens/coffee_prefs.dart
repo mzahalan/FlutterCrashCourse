70a51a0c57c13e4e23f72b832de7f8ff6161ed8a
Mike Zahalan <mzahalan@amazon.com>
2025-06-13 14:44:20 -0700

    Between Lessons -
    - Added a couple plus buttons, I think we'll want minus buttons too.
    
    - For Strength and Sugars, I repeat the image X times instead of put the count in a Text box. I'm guessing this is where Shaun is going with this.

--- coffee_card/lib/screens/coffee_prefs.dart
+++ coffee_card/lib/screens/coffee_prefs.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class CoffeePrefs extends StatelessWidget {
   const CoffeePrefs({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -10,29 +10,33 @@ class CoffeePrefs extends StatelessWidget {
         Row(
           children: [
             const Text('Strength: '),
-            const Text('3'),
-            Image.asset(
-              'assets/img/coffee_bean.png',
-              width: 25,
-              color: Colors.brown[100],
-              colorBlendMode: BlendMode.multiply,
-            ),
+            for(int i = 0; i < 3; i++)
+              Image.asset(
+                'assets/img/coffee_bean.png',
+                width: 25,
+                color: Colors.brown[100],
+                colorBlendMode: BlendMode.multiply,
+              ),
             const Expanded(child: SizedBox()),
-            const Text('+'),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('+')),
           ],
         ),
         Row(
           children: [
             const Text('Sugars: '),
-            const Text('3'),
-            Image.asset(
-              'assets/img/sugar_cube.png',
-              width: 25,
-              color: Colors.brown[100],
-              colorBlendMode: BlendMode.multiply,
-            ),
+            for(int i = 0; i < 3; i++)
+              Image.asset(
+                'assets/img/sugar_cube.png',
+                width: 25,
+                color: Colors.brown[100],
+                colorBlendMode: BlendMode.multiply,
+              ),
             const Expanded(child: SizedBox()),
-            const Text('+'),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('+')),
           ],
         ),
       ],
